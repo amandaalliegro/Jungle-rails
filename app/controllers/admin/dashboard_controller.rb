4f93f0524ee003dcdf5b75a4a3f648c7c233f61e
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-11-12 20:42:48 +0000

    Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
+    @products_per_category = Category.select(:id, :name).distinct.map do |category|
+      {id: category[:id], name: category[:name], count: category.products.count}
   end
 end
